dbd8b390625d93b6ea3f2eb779633faee43bb580
Seva Poliakov <seva@ctrlok.com>
2023-05-13 20:14:54 +0300

    start working on clicking

--- ActiveThings/ContentView.swift
+++ ActiveThings/ContentView.swift
@@ -3,18 +3,42 @@ import Foundation
 
 
 // Separate the logic for fetching to-dos into a struct
+struct ToDo: Codable {
+    let recordID: String
+    let RecordName: String
+}
+
 struct ThingsManager {
     static let shared = ThingsManager()
     
     private init() {}
     
     func fetchToDos(completion: @escaping (String) -> Void) {
-        let executeAppleScript = ExecuteAppleScript()
-        executeAppleScript.execute { (status, result) in
-            if status == "Execution of AppleScript successful!" {
-                completion(result)
-            } else {
-                completion("Error: \(status)")
+        DispatchQueue.global().async {
+            let executeAppleScript = ExecuteAppleScript()
+            executeAppleScript.execute { (status, result) in
+                print("Status: \(status)") // Print the status
+                print("Result: \(result)") // Print the result
+                
+                DispatchQueue.main.async {
+                    if status == "Execution of AppleScript successful!" {
+                        if let jsonData = result.data(using: .utf8) {
+                            let decoder = JSONDecoder()
+                            do {
+                                let todo = try decoder.decode(ToDo.self, from: jsonData)
+                                completion("\(todo.recordID): \(todo.RecordName)")
+                            } catch {
+                                print("Error: \(error.localizedDescription)") // Print the error details
+                                completion("Error: Unable to decode JSON")
+                            }
+                        } else {
+                            completion(result)
+                        }
+                    } else {
+                        completion("Error: \(status)")
+                    }
+                    
+                }
             }
         }
     }
@@ -22,25 +46,43 @@ struct ThingsManager {
 
 struct ContentView: View {
     @State private var firstToDo: String = ""
-    
+
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            RadialGradient(gradient: Gradient(colors: [Color.black.opacity(0.5), Color.black.opacity(0.0)]), center: .topLeading, startRadius: 0, endRadius: 250)
-                .edgesIgnoringSafeArea(.all)
+        GeometryReader { geometry in
+            ZStack(alignment: .topLeading) {
+                Rectangle()
+                    .fill(RadialGradient(gradient: Gradient(colors: [Color.black.opacity(0.8), Color.black.opacity(0.0)]),
+                                         center: UnitPoint(x: (geometry.frame(in: .local).midX - 320) / geometry.size.width,
+                                                           y: (geometry.frame(in: .local).midY - 320) / geometry.size.height),
+                                         startRadius: 0, endRadius: 550))
+                    .scaleEffect(x: 1, y: 0.2, anchor: .topLeading) // Adjust the y value to change the vertical scaling
+                    .edgesIgnoringSafeArea(.all)
+                    .zIndex(-1) // Set the zIndex of the gradient to -1
+                
+                Text(firstToDo)
+                    .font(.system(size: 18, weight: .regular))
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 25, leading: 40, bottom: 0, trailing: 0))
+                    .overlay(
+                        LinearGradient(gradient: Gradient(stops: [
+                            Gradient.Stop(color: Color(red: 0.98, green: 0.98, blue: 0.98), location: 0),
+                            Gradient.Stop(color: Color.white, location: 80 / geometry.size.width),
+                            Gradient.Stop(color: Color.white, location: 300 / geometry.size.width),
+                            Gradient.Stop(color: Color(red: 0.92, green: 0.92, blue: 0.92).opacity(1), location: 1)
+                        ]), startPoint: .leading, endPoint: .trailing)
+                    )
+                    .mask(Text(firstToDo).font(.system(size: 18, weight: .regular))
+                    )
             
-            
-            Text(firstToDo)
-                .font(.system(size: 18, weight: .medium))
-                .foregroundColor(.white)
-                .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 0))
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onAppear(perform: startFetchingToDos)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .onAppear(perform: startFetchingToDos)
     }
     
     func startFetchingToDos() {
         fetchToDos()
-        Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { _ in
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             fetchToDos()
         }
     }
@@ -103,30 +145,33 @@ struct MyApp: App {
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var window: NSWindow?
+    var windows: [NSWindow] = []
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
 
-        // Create the window and set the content view.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 300, height: 300),
-            styleMask: [.borderless], // borderless window
-            backing: .buffered, defer: false)
-        window?.center()
-        window?.setFrameAutosaveName("Main Window")
-        window?.contentView = NSHostingView(rootView: contentView)
-        window?.level = NSWindow.Level(rawValue: -1000) // Change this line to place the widget in the background
-        window?.collectionBehavior = [.canJoinAllSpaces, .managed, .fullScreenAuxiliary]
-        window?.isOpaque = false
-        window?.backgroundColor = NSColor.clear
+        for screen in NSScreen.screens {
+            // Create the window and set the content view.
+            let window = NSWindow(
+                contentRect: NSRect(x: 0, y: 0, width: 300, height: 300),
+                styleMask: [.borderless], // borderless window
+                backing: .buffered, defer: false)
+            window.center()
+            window.setFrameAutosaveName("Main Window")
+            window.contentView = NSHostingView(rootView: contentView)
+            window.level = NSWindow.Level(rawValue: -1000) // Change this line to place the widget in the background
+            window.collectionBehavior = [.canJoinAllSpaces, .managed, .fullScreenAuxiliary]
+            window.isOpaque = false
+            window.backgroundColor = NSColor.clear
 
-        let screenSize = window?.screen?.visibleFrame.size ?? NSSize(width: 800, height: 600) // Use visibleFrame to exclude the menu bar
-        let windowSize = NSSize(width: 500, height: 500) // Adjust this to your liking
-        let windowOrigin = NSPoint(x: 0, y: screenSize.height - windowSize.height + 2 ) // Top-left corner, right under the menu bar
-        window?.setFrame(NSRect(origin: windowOrigin, size: windowSize), display: true)
+            let screenSize = screen.visibleFrame.size // Use visibleFrame to exclude the menu bar
+            let windowSize = NSSize(width: 500, height: 500) // Adjust this to your liking
+            let windowOrigin = NSPoint(x: screen.visibleFrame.origin.x, y: screenSize.height + screen.visibleFrame.origin.y - windowSize.height + 2) // Top-left corner, right under the menu bar
+            window.setFrame(NSRect(origin: windowOrigin, size: windowSize), display: true)
 
-        window?.makeKeyAndOrderFront(nil)
+            window.makeKeyAndOrderFront(nil)
+            windows.append(window)
+        }
     }
 }
--- ActiveThings/fetchToDos.scpt
+++ ActiveThings/fetchToDos.scpt
@@ -1,19 +1,19 @@
-#!/bin/sh
-
-#  fetchToDos.scpt
-#  ActiveThings
-#
-#  Created by Seva Poliakov on 13.05.2023.
-#  
-
+use framework "Foundation"
 
 tell application "Things3"
     set todoList to {}
     repeat with inboxToDo in to dos of list "Today"
-        copy name of inboxToDo to the end of todoList
+        set todoRecord to {recordID:id of inboxToDo, RecordName:name of inboxToDo}
+        copy todoRecord to the end of todoList
     end repeat
     if length of todoList > 0 then
-        return item 1 of todoList
+        set firstToDo to item 1 of todoList
+        set myObject to firstToDo
+        
+        set jsonString to current application's NSJSONSerialization's dataWithJSONObject:myObject options:0 |error|:(missing value)
+        set jsonString to current application's NSString's alloc()'s initWithData:jsonString encoding:(current application's NSUTF8StringEncoding)
+        set jsonString to jsonString as text
+        return jsonString
     else
         return "No To-Dos"
     end if
