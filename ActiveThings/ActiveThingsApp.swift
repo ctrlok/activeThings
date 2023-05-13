333df62ec95d2679d9b17fb6bf23ad69d8fac190
Seva Poliakov <seva@ctrlok.com>
2023-05-13 09:27:54 +0300

    Initial Commit

---
+++ ActiveThings/ActiveThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveThingsApp.swift
+//  ActiveThings
+//
+//  Created by Seva Poliakov on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ActiveThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
